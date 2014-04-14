a7abb220d61b44c699a67bb723baf143277c759f
jakabszili13 <jakabszili15@gmail.com>
2014-04-15 01:39:30 +0200

    go

--- src/main.java
+++ src/main.java
@@ -3,5 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("r2_main");
 	}
 }
